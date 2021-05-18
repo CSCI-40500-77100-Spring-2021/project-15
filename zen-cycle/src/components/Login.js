c7c8ad11fa9bbd483a9be98e1425b75c488b58a4
edward-delossantos <35975985+edward-delossantos@users.noreply.github.com>
2021-05-18 18:52:52 -0400

    added email verification for users

--- zen-cycle/src/components/Login.js
+++ zen-cycle/src/components/Login.js
@@ -1,58 +1,68 @@
-import React, { useRef, useState } from 'react'
-import { Form, Button, Card, Alert } from 'react-bootstrap'
-import  { useAuth } from '../contexts/AuthContext'
-import { Container } from 'react-bootstrap'
-import { Link, useHistory } from 'react-router-dom'
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Container } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 
-export default function Login(){
-    const emailRef = useRef()
-    const passRef = useRef()
-    const { login, currentUser } = useAuth()
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState(false)
-    const history = useHistory()
+export default function Login() {
+  const emailRef = useRef();
+  const passRef = useRef();
+  const { login, currentUser, verifyEmail } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
-    async function handleSubmit(e){
-        e.preventDefault()
-        try {
-            setError('')
-            setLoading(true)
-            await login(emailRef.current.value, passRef.current.value)
-            history.push("/")
-        } catch{
-            setError('Sign in Failed.')
-        }
-        setLoading(false)
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passRef.current.value);
+      if (currentUser?.emailVerified) {
+        // history.push("/");
+      } else {
+        setError("Email needs to be verified");
+        verifyEmail();
+      }
+    } catch {
+      setError("Sign in Failed.");
     }
+    setLoading(false);
+  }
 
-    return(
-        <>
-        <Container ClassName='d-flex align-items-center justify-content-center'>
-            <div className="w-100" style={{maxWidth:"400px", margin: "0 auto"}}>
-            <Card>
-                <Card.Body>
-                    <h2 className='text-center mb-4'>Log In</h2>
-                    {currentUser.email}
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required />
-                        </Form.Group>
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passRef} required />
-                        </Form.Group>
-                        <Button disabled={loading} className="w-100" type="submit">Log In</Button>
-                        
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className='w-100 text-center mt-2'>
-                No Account? <Link to="/signup">Sign Up</Link>
-            </div>
-            </div>
-        </Container> 
-        </>
-    )
+  return (
+    <>
+      <Container ClassName="d-flex align-items-center justify-content-center">
+        <div className="w-100" style={{ maxWidth: "400px", margin: "0 auto" }}>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Log In</h2>
+              {currentUser?.emailVerified ? (
+                <>{currentUser?.email}</>
+              ) : (
+                <></>
+              )}
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" ref={passRef} required />
+                </Form.Group>
+                <Button disabled={loading} className="w-100" type="submit">
+                  Log In
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <div className="w-100 text-center mt-2">
+            No Account? <Link to="/signup">Sign Up</Link>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
--- zen-cycle/src/contexts/AuthContext.js
+++ zen-cycle/src/contexts/AuthContext.js
@@ -1,41 +1,55 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { auth } from '../firebase'
+import React, { useContext, useState, useEffect } from "react";
+import { auth } from "../firebase";
 
+const AuthContext = React.createContext();
 
-const AuthContext = React.createContext()
-
-export function useAuth(){
-    return useContext(AuthContext)
+export function useAuth() {
+  return useContext(AuthContext);
 }
 
-export function AuthProvider({children}){
-    const [currentUser, setCurrentUser] = useState()
-    const [loading,setLoading] = useState(true)
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
-    function signup(email, password){
-        return auth.createUserWithEmailAndPassword(email,password)
-    }
-    function login(email, password){
-        return auth.signInWithEmailAndPassword(email,password)
-    }
+  async function signup(email, password) {
+    const userCredentials = await auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+    await userCredentials.sendEmailVerification();
+    return userCredentials;
+  }
 
-    useEffect(()=>{
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            setCurrentUser(user)
-            setLoading(false)
-        })
-        return unsubscribe
-    }, [])
+  async function login(email, password) {
+    const userCredentials = await auth.signInWithEmailAndPassword(
+      email,
+      password
+    );
+    return userCredentials;
+  }
 
-    const value ={
-        currentUser,
-        login,
-        signup
-    }
+  function verifyEmail() {
+    currentUser.sendEmailVerification();
+  }
 
-    return (
-            <AuthContext.Provider value={value}>
-                {!loading && children}
-            </AuthContext.Provider>
-    )
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    login,
+    signup,
+    verifyEmail,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
